cc56e479bcfea7a47f01369358d280083de49eec
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-02-12 17:38:36 +1100

    Expirmenting with setting up React

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 require("dotenv").config();
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./src/App.css";
+
+function App() {
+  let temperature = 19;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <h2>The temperature is {temperature}</h2>
+    </div>
+  );
+}
+
+const root = ReactDOM(document.getElementById("root"));
+root.render(<App />);
